56b5b428e7652b44d3d5906827f9701dfd714b7f
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-11-03 16:01:41 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('.')); // for index.html
+
+require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+console.log('App started on localhost.');
