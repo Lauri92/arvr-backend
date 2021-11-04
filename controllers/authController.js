7ef3cee9a09e533ab2776989e386a42ad40d719c
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-11-04 22:41:57 +0200

    simple check

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,11 +7,23 @@ const get_message = async (req, res, next) => {
 };
 
 const post_user = async (req, res, next) => {
+
   // Extract the validation errors from a request.
   const errors = validationResult(req);
-  console.log(`req.body: ${req.body}`);
-  console.log(`errors: ${errors}`);
-  res.status(200).send({message: 'post_user'});
+
+  try {
+    if (!errors.isEmpty()) {
+      // Errors in validation
+      console.log('user create error', errors);
+      res.status(200).send({message: 'Errors in validation!'});
+    } else {
+      // No errors
+      console.log('user create no errors!');
+      res.status(200).send({message: 'No errors in validation!'});
+    }
+  } catch (e) {
+    console.error('e:', e);
+  }
 };
 
 module.exports = {
