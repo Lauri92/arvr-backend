5a20cbf4c05950390df36741ec865e115bd7a812
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-11-04 21:59:59 +0200

    validation result

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,20 @@
 'use strict';
+const {validationResult} = require('express-validator');
 
 const get_message = async (req, res, next) => {
   console.log(req);
-  res.status(200).send({message: "get_message"});
+  res.status(200).send({message: 'get_message'});
 };
 
 const post_user = async (req, res, next) => {
-  console.log(req);
-  res.status(200).send({message: "post_user"})
-}
-
+  // Extract the validation errors from a request.
+  const errors = validationResult(req);
+  console.log(`req.body: ${req.body}`);
+  console.log(`errors: ${errors}`);
+  res.status(200).send({message: 'post_user'});
+};
 
 module.exports = {
   get_message,
   post_user,
-}
+};
