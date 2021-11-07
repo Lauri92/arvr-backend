f619d5ae208604b2b48b18ad6d3e656c6ccda0e8
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-11-07 12:55:40 +0200

    more try catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,49 +8,56 @@ const get_message = async (req, res) => {
 };
 
 const create_user = async (req, res) => {
-  // Extract the validation errors from a request.
-  const errors = validationResult(req);
-  const lowerCaseBodyUsername = req.body.username.toLowerCase()
-  const bodyUsernameUppercase = lowerCaseBodyUsername.charAt(0).
-          toUpperCase() +
-      lowerCaseBodyUsername.slice(1);
+  try {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    const lowerCaseBodyUsername = req.body.username.toLowerCase();
+    const bodyUsernameUppercase = lowerCaseBodyUsername.charAt(0).
+            toUpperCase() +
+        lowerCaseBodyUsername.slice(1);
 
-  const checkDuplicate = await Schemas.arUserModel.findOne(
-      {username: `${bodyUsernameUppercase}`})
+    const checkDuplicate = await Schemas.arUserModel.findOne(
+        {username: `${bodyUsernameUppercase}`});
 
-  //console.log('\x1b[36m%s\x1b[0m', `${req.body.username.toLowerCase()}`);
+    //console.log('\x1b[36m%s\x1b[0m', `${req.body.username.toLowerCase()}`);
 
-  if (checkDuplicate === undefined || checkDuplicate === null) {
-    // Username doesn't exist
-    try {
-      if (!errors.isEmpty()) {
-        // Errors in validation
-        console.log('Errors!!', errors);
-        res.status(200).json({message: 'Errors in validation!'});
-      } else {
-        // No errors, salt and hash pw
-        const salt = bcrypt.genSaltSync(10);
-        req.body.password = bcrypt.hashSync(req.body.password, salt);
-        const user = {
-          username: bodyUsernameUppercase,
-          password: req.body.password,
-        };
+    if (checkDuplicate === undefined || checkDuplicate === null) {
+      // Username doesn't exist
+      try {
+        if (!errors.isEmpty()) {
+          // Errors in validation
+          console.log('Errors!!', errors);
+          const mappederrors = errors.errors.map((error) => {
+            return `Input field: ${error.param}, error: ${error.msg}\n`;
+          });
+          res.status(200).json({message: `${mappederrors}`});
+        } else {
+          // No errors, salt and hash pw
+          const salt = bcrypt.genSaltSync(10);
+          req.body.password = bcrypt.hashSync(req.body.password, salt);
+          const user = {
+            username: bodyUsernameUppercase,
+            password: req.body.password,
+          };
 
-        try {
-          await Schemas.arUserModel.create(user);
-          res.status(200).json({message: `${req.body.username} inserted!`});
-        } catch (e) {
-          res.status(200).json({message: 'We failed to insert'});
+          try {
+            await Schemas.arUserModel.create(user);
+            res.status(200).json({message: `${req.body.username} inserted!`});
+          } catch (e) {
+            res.status(200).json({message: 'We failed to insert'});
+          }
+
         }
-
+      } catch (e) {
+        console.error('e:', e);
+        res.status(400).json({message: 'Error inserting user!'});
       }
-    } catch (e) {
-      console.error('e:', e);
-      res.status(400).json({message: 'Error inserting user!'});
+    } else {
+      // Username exists
+      res.status(200).json({message: 'User name already exists!'});
     }
-  } else {
-    // Username exists
-    res.status(200).json({message: 'User name already exists!'});
+  } catch (e) {
+    res.status(200).json({message: 'Failed to insert 😣'});
   }
 
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-//const authController = require('../Controllers/authController');
 const {body} = require('express-validator');
 const authController = require('../controllers/authController');
 
