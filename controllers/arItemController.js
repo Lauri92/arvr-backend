27b7c50a94bd68e74a68a3fda37b0b3670687e5d
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-11-08 14:49:28 +0200

    removed logs causing crash on remote server

--- controllers/arItemController.js
+++ controllers/arItemController.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const getSecuredItem = async (req, res) => {
-  console.log(req.user.tokenUser.id);
-  console.log(req.headers.authorization);
   res.status(200).send({message: 'Get secured item'});
 };
 
