8ee204c206e2aabc95ce4210b5ecd47c0c401bc4
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-11-24 16:55:19 +0200

    changes in fileFilter

--- utils/multerUtils.js
+++ utils/multerUtils.js
@@ -26,14 +26,16 @@ const upload3d = multer({
         return cb(null, false, new Error('Not an image!'));
       }
     } else if (file.fieldname === 'bin') {
-      if (file.mimetype.includes('octet-stream')) {
+      if (file.mimetype.includes('octet-stream') ||
+          file.mimetype.includes('application/macbinary')) {
         return cb(null, true);
       } else {
         return cb(null, false, new Error('Not a bin file!'));
       }
     } else if (file.fieldname === 'gltf') {
       console.log(file);
-      if (file.mimetype.includes('gltf')) {
+      if (file.mimetype.includes('gltf') ||
+          file.mimetype.includes('application/octet-stream')) {
         return cb(null, true);
       } else {
         return cb(null, false, new Error('Not a gltf file!'));
